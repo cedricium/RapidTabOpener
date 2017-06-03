81e1d38ac0d1e252009e71c554bc1e8826c0f159
Cedricium <amaya.cedric@gmail.com>
2017-06-03 16:00:51 -0700

    Window-type validation functionality complete

--- background.js
+++ background.js
@@ -1,35 +1,61 @@
+/**
+ * RapidTabOpener
+ * 
+ * Action to be executed when toolbar button is clicked. Determines the
+ * current window mode and from there will either immediately open
+ * the desired tabs or first open a new incognito window then open
+ * the tabs.
+ *
+ * @author Cedric Amaya
+ */
+
 browser.browserAction.onClicked.addListener(executeByWindowType);
 
-function determineWindowMode() {
-  var window = browser.windows.getCurrent();
-  return window;
-}
-
+/**
+ * Determines the current window's mode (incognito or not) and with
+ * that information, will either execute the openTabs() method or
+ * close current window and execute the openIncognitoWindow() method.
+ */
 function executeByWindowType(window) {
+  // debugging window object
+  console.log(window);
   
   // debugging window mode
   console.log(window.incognito);
   
+  // if current window is incognito, just open desired tabs
   if (window.incognito === true)
     openTabs();
-  else  // window is not in incognito mode, so open incognito window
+  
+  // if current window is not incognito, do the following...
+  else {
+    // current window is not incognito, so it gets closed
+    var closeWindow = browser.windows.remove(window.windowId);
     
-    /* TODO:
-     *   - create object of tabs to create
-     *   - pass above object to window.create to open tabs
-     */
-    
-    // debugging
-//    var incognitoWindow = browser.windows.create({
-//      url: [
-//        "https://gmail.com",
-//        "https://facebook.com"
-//      ],
-//      incognito: true,
-//      state: "maximized"
-//    });
+    openIncognitoWindow();
+  }
 }
 
+/**
+ * Opens a new incognito window then populates tabs using the
+ * openTabs() method.
+ */
+function openIncognitoWindow() {
+  // new incognito window is opened - a Promise is returned
+  var incognitoWindow = browser.windows.create({
+    incognito: true,
+    state: "maximized"
+  });
+
+  // once Promise (which returns a Window object) is fulfilled,
+  //    openTabs() method is triggered in new incognito window
+  incognitoWindow.then(openTabs);
+}
+
+/**
+ * Creates a series of tabs with websites most used by myself
+ * at work.
+ */
 function openTabs() {  
   var a = browser.tabs.create({
         active: true,
