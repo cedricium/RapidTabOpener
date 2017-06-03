7c1d840ef36c9ff6aa3665511fb76c2e16ff1575
Cedricium <amaya.cedric@gmail.com>
2017-06-03 01:09:52 -0700

    Adding functionality to open new incognito window if not already in one

--- background.js
+++ background.js
@@ -1,6 +1,36 @@
-browser.browserAction.onClicked.addListener(openTabs);
+browser.browserAction.onClicked.addListener(executeByWindowType);
 
-function openTabs() {
+function determineWindowMode() {
+  var window = browser.windows.getCurrent();
+  return window;
+}
+
+function executeByWindowType(window) {
+  
+  // debugging window mode
+  console.log(window.incognito);
+  
+  if (window.incognito === true)
+    openTabs();
+  else  // window is not in incognito mode, so open incognito window
+    
+    /* TODO:
+     *   - create object of tabs to create
+     *   - pass above object to window.create to open tabs
+     */
+    
+    // debugging
+//    var incognitoWindow = browser.windows.create({
+//      url: [
+//        "https://gmail.com",
+//        "https://facebook.com"
+//      ],
+//      incognito: true,
+//      state: "maximized"
+//    });
+}
+
+function openTabs() {  
   var a = browser.tabs.create({
         active: true,
         index: 0,
