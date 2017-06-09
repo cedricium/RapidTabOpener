496a373bb057f46e2d191a20f3f7d8428e3a81ba
Cedricium <amaya.cedric@gmail.com>
2017-06-08 22:28:19 -0700

    Completed code rework to fix issue #1

--- background.js
+++ background.js
@@ -2,112 +2,13 @@
  * RapidTabOpener
  * 
  * Action to be executed when toolbar button is clicked. Determines the
- * current window mode and from there will either immediately open
- * the desired tabs or first open a new incognito window then open
- * the tabs.
+ * current window mode and the desired window type then with that
+ * information either opens a new window first or the specified URLs.
  *
- * @author Cedric Amaya
+ * @author cedricium - Cedric Amaya
  */
 
 browser.browserAction.onClicked.addListener(getDataFromStorage);
-
-/**
- * Determines the current window's mode (incognito or not) and with
- * that information, will either execute the openTabs() method or
- * close current window and execute the openIncognitoWindow() method.
- */
-function executeByWindowType(window) {
-  // debugging window object
-  console.log(window);
-  
-  // debugging window mode
-  console.log(window.incognito);
-  
-  // if current window is incognito, just open desired tabs
-  if (window.incognito === true)
-    openTabs();
-  
-  // if current window is not incognito, do the following...
-  else {
-    // current window is not incognito, so it gets closed
-    var closeWindow = browser.windows.remove(window.windowId);
-    
-    openIncognitoWindow();
-  }
-}
-
-/**
- * Opens a new incognito window then populates tabs using the
- * openTabs() method.
- */
-function openIncognitoWindow() {
-  // new incognito window is opened - a Promise is returned
-  var incognitoWindow = browser.windows.create({
-    incognito: true,
-    state: "maximized"
-  });
-
-  // once Promise (which returns a Window object) is fulfilled,
-  //    openTabs() method is triggered in new incognito window
-  incognitoWindow.then(openTabs);
-}
-
-/**
- * Creates a series of tabs with websites most used by myself
- * at work.
- */
-function openTabs() {  
-//  var a = browser.tabs.create({
-//        active: true,
-//        index: 0,
-//        url: "https://workforcenow.adp.com"
-//      }),
-//      
-//      b = browser.tabs.create({
-//        active: false,
-//        index: 1,
-//        url: "https://gmail.com"
-//      }),
-//      
-//      c = browser.tabs.create({
-//        active: false,
-//        index: 2,
-////        url: " TESTRAILS URL HERE "
-//      }),
-//
-//      d = browser.tabs.create({
-//        active: false,
-//        index: 3,
-//        url: "http://jira.surfcrew.com"
-//      });
-  
-  var urls = browser.storage.local.get(["urls", "windowSettings"]);
-  urls.then(onGot);
-}
-
-//function onGot(item) {
-//  console.log(item.windowSettings); 
-//  
-//  var urls = item.urls;
-//  
-//  // create tabs based off locally-store URLs
-//  for (var i = 0; i < urls.length; i++) {
-//    var url = urls[i];
-//    console.log(url);
-//    
-//    browser.tabs.create({
-//      index: i,
-//      url: url
-//    });
-//  }
-//}
-
-
-
-/*
-=====================  TESTING BELOW  ==========================
-===========  v1.1.1 - Selection of window type  ================
-*/
 
 var urls,
     windowSettings,
@@ -126,24 +27,32 @@ function determineWindow(item) {
   
   var windowType = windowSettings.type;
   
-  console.log("window_type_requested: " + windowType);
+  if (urls.length <= 0)
+    openOptionsPage();
+  else {
+    if (windowType === "incognito" && currentWindow.incognito)
+      openURLs();
+      
+    if (windowType === "incognito" && !currentWindow.incognito)
+      openWindow(windowType);
+      
+    if (windowType === "normal" && !currentWindow.incognito)
+      openURLs();
+    
+    if (windowType === "normal" && currentWindow.incognito)
+      openWindow(windowType);
+  }
+}
+
+function openOptionsPage() {
+  browser.runtime.openOptionsPage();
   
-  if (windowType === "incognito" && currentWindow.incognito) {
-    console.log("window_type_determined_path: " + 1);
-    openURLs();
-  }
-  if (windowType === "incognito" && !currentWindow.incognito) {
-    console.log("window_type_determined_path: " + 2);
-    openWindow(windowType);
-  }
-  if (windowType === "normal" && !currentWindow.incognito) {
-    console.log("window_type_determined_path: " + 3);
-    openURLs();
-  }
-  if (windowType === "normal" && currentWindow.incognito) {
-    console.log("window_type_determined_path: " + 4);
-    openWindow(windowType);
-  }
+  browser.notifications.create({
+    "type": "basic",
+    "title": "Zoinks!",
+    "message": "Add some URLs that you want opened.",
+    "iconUrl": "icons/tabs-128.png"
+  });
 }
 
 function openWindow(type) {
@@ -155,7 +64,7 @@ function openWindow(type) {
       state: "maximized"
     });
   }
-  
+
   else if (type === "incognito") {
     newWindow = browser.windows.create({
       incognito: true,
@@ -169,7 +78,6 @@ function openWindow(type) {
 function openURLs() {
   for (var i = 0; i < urls.length; i++) {
     var url = urls[i];
-    console.log(url);
     
     browser.tabs.create({
       index: i,
